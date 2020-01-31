89afae53167b8c28aed681eb5d3c901b502090f3
Christopher Santana <christopher.santana@nav.no>
2020-01-31 11:18:18 +0100

    Added option to specify maven settings file.

--- build.sh
+++ build.sh
@@ -12,14 +12,18 @@ if [[ $INPUT_QUIET ]]; then
   QUIET="--quiet"
 fi
 
+if [[ -f $INPUT_MAVEN_SETTINGS_FILE ]]; then
+  MAVEN_SETTINGS="-s $INPUT_MAVEN_SETTINGS_FILE"
+fi
+
 if [[ -f "gradlew" ]]; then
   ./gradlew $GRADLE_CMD $QUIET
 elif [[ -f "build.gradle" || -f "build.gradle.kts" ]]; then
   gradle $GRADLE_CMD $QUIET
 elif [[ -f "mavenw" ]]; then
-  ./mavenw $MAVEN_CMD $QUIET
+  ./mavenw $MAVEN_CMD $QUIET $MAVEN_SETTINGS
 elif [[ -f "pom.xml" ]]; then
-  mvn $MAVEN_CMD $QUIET
+  mvn $MAVEN_CMD $QUIET $MAVEN_SETTINGS
 else
   echo "This doesn't look like a maven or gradle project."
 fi
