64b2f4d0bd732135f5e5c8d8da8c0e271ef159ba
Christopher Santana <christopher.santana@nav.no>
2020-03-27 11:46:15 +0100

    Ikke default til å tagge latest hvis workflowen kjører i master.

--- docker-publish/entrypoint.sh
+++ docker-publish/entrypoint.sh
@@ -5,27 +5,17 @@ if [[ -z "$INPUT_TAG_NAME" ]]; then
   INPUT_TAG_NAME="$(git log -1 --pretty='%ad' --date=format:'%Y%m%d%H%M%S')-$(git log -1 --pretty='%h')"
 fi
 
-# Tag 'latest' only for master unless explicitly overridden
-if [[ "$INPUT_TAG_LATEST" = "false" ]]; then
-  TAG_LATEST=false
-elif [[ "$INPUT_TAG_LATEST" = "true" || "$GITHUB_REF" = "refs/heads/master" ]]; then
-  TAG_LATEST=true
-else
-  TAG_LATEST=false
-fi
-
 # Create image names
 APP_NAME=$(echo $GITHUB_REPOSITORY | rev | cut -f1 -d"/" | rev )
 IMAGE_BASE="docker.pkg.github.com/$GITHUB_REPOSITORY/$APP_NAME"
 IMAGE_TAGGED="$IMAGE_BASE:$INPUT_TAG_NAME"
 IMAGE_LATEST="$IMAGE_BASE:latest"
 
-
 # Set basic auth
 echo $INPUT_GITHUB_TOKEN | docker login docker.pkg.github.com -u $GITHUB_REPOSITORY --password-stdin
 
 # Build and push docker images
-if $TAG_LATEST; then
+if [[ $INPUT_TAG_LATEST = "true" ]]; then
   docker build --tag $IMAGE_TAGGED --tag $IMAGE_LATEST .
   docker push $IMAGE_TAGGED
   docker push $IMAGE_LATEST
